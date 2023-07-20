4d312687b37f809c9f496407d7186f8a054da6bf
Jiwon Kim <jiwon.kim4@gmail.com>
2023-07-20 14:32:58 +0900

    first commit

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+// import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "../../../lib/mongodb"
+
+console.log(process.env.GOOGLE_ID)
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  adaptor: MongoDBAdapter(clientPromise),
+})
